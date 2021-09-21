c0a694ce81641620ffbbbc64b113bb190bb30347
Zhi Qi <qizhi@pingcap.com>
2021-09-22 02:50:20 +0800

    init project.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from manimlib import *
 
+COLOR_DRIVER = RED
+COLOR_FIXED = BLUE
+COLOR_DRIVEN = YELLOW
+COLOR_TARGET = GREEN
+
+# TODO: find a better way to run and debug.
+# from manimlib.__main__ import main
+# if __name__ == '__main__':
+#     main()
+
 
 class Linkage0(Scene):
     def construct(self):
         radius = 2
 
-        fixed = Dot(ORIGIN)
-        driver = Dot(RIGHT * radius)
+        fixed = Dot(ORIGIN).set_color(COLOR_FIXED)
+        driver = Dot(RIGHT * radius).set_color(COLOR_DRIVER)
         line = Line(fixed, driver)
 
+        # TODO: put line to lower-level.
+
         def update_func(mob):
-            #mob.set_start_and_end_attrs(driver, driver)
             mob.put_start_and_end_on(fixed.get_center(), driver.get_center())
 
         self.add(fixed, driver, line)
@@ -22,7 +33,5 @@ class Linkage0(Scene):
                 MoveAlongPath(driver, Circle(radius=radius)),
                 UpdateFromFunc(line, update_func)
             )
+        # TODO: draw locus.
 
-# from manimlib.__main__ import main
-# if __name__ == '__main__':
-#     main()
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Linkage
+
+Thanks to [manim](https://github.com/3b1b/manim), great math print lib. 
+
+Thanks to [为什么连杆可以画出曲线](https://www.bilibili.com/medialist/play/594380494?from=space&business=space&sort_field=pubtime), great Popular Science video for linkage.
+
+## Run
+
+1. [Install manim](https://github.com/3b1b/manim#installation).
+
+2. Run the linkages by `manimgl <source> <scene>` like:
+```sh
+manimgl main.py Linkage0
+```
+
+## TODO
+
+See TODO in codes.
+
+## Agreement
+
+### dot
+
+- color red: the driver, move around a circle.
+- color yellow: point that will be driven.
+- color blue: fixed point.
+- color green: driven point used to draw the locus.
